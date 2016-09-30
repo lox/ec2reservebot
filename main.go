090b626b6967745f16a62c803f729b27cbe162a8
Lachlan Donald <lachlan@ljd.cc>
2016-09-30 15:28:06 +1000

    Get aws pricing for comparison on offerings

--- main.go
+++ main.go
@@ -4,11 +4,72 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"reflect"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
+
+const (
+	durationOneYear   = 31536000
+	durationThreeYear = 94608000
+)
+
+func getAWSOffering(svc *ec2.EC2, offering *ec2.ReservedInstancesOffering) (*ec2.ReservedInstancesOffering, error) {
+	duration := *offering.Duration
+
+	if duration <= durationOneYear {
+		duration = durationOneYear
+	} else {
+		duration = durationThreeYear
+	}
+
+	output, err := svc.DescribeReservedInstancesOfferings(&ec2.DescribeReservedInstancesOfferingsInput{
+		ProductDescription: offering.ProductDescription,
+		IncludeMarketplace: aws.Bool(false),
+		InstanceType:       offering.InstanceType,
+		InstanceTenancy:    offering.InstanceTenancy,
+		AvailabilityZone:   offering.AvailabilityZone,
+		OfferingClass:      offering.OfferingClass,
+		OfferingType:       offering.OfferingType,
+		MinDuration:        &duration,
+		MaxDuration:        &duration,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if found := len(output.ReservedInstancesOfferings); found > 2 {
+		log.Printf("%#v", output.ReservedInstancesOfferings)
+		return nil, fmt.Errorf("Expected 1 offering, found %d", found)
+	} else if found == 2 && !reflect.DeepEqual(output.ReservedInstancesOfferings[0],
+		output.ReservedInstancesOfferings[1]) {
+		return nil, fmt.Errorf("Expected 1 offering, found %d", found)
+	} else if found == 0 {
+		return nil, fmt.Errorf("No AWS offering found for %s", getAssetName(offering))
+	}
+
+	return output.ReservedInstancesOfferings[0], nil
+}
+
+func getAssetName(offering *ec2.ReservedInstancesOffering) string {
+	duration := "1yr"
+
+	if *offering.Duration == durationThreeYear {
+		duration = "3yr"
+	}
+
+	return fmt.Sprintf("%s:%s:%s:%s:%s.tenancy:%s.offering",
+		*offering.InstanceType,
+		duration,
+		strings.Replace(strings.ToLower(*offering.ProductDescription), " ", ".", -1),
+		strings.Replace(strings.ToLower(*offering.OfferingType), " ", ".", -1),
+		*offering.InstanceTenancy,
+		*offering.OfferingClass,
+	)
+}
 
 func pageInstanceOfferings(svc *ec2.EC2, f func(offering *ec2.ReservedInstancesOffering) error) (n int, err error) {
 	var ids = map[string]struct{}{}
@@ -79,7 +140,12 @@ func main() {
 	log.Printf("Finding reserved instance offerings")
 
 	n, err := pageInstanceOfferings(svc, func(offering *ec2.ReservedInstancesOffering) error {
-		log.Printf("Storing %s", *offering.ReservedInstancesOfferingId)
+		_, err := getAWSOffering(svc, offering)
+		if err != nil {
+			log.Printf("Error getting offering: %v", err)
+			return nil
+		}
+		log.Printf("Storing %s (%s)", *offering.ReservedInstancesOfferingId, getAssetName(offering))
 		return db.StoreOffering(offering)
 	})
 	if err != nil {
